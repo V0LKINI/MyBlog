bed63e5f7924590443906de8f7d763f78339f931
Кирилл Волков <k.volkov.n@gmail.com>
2020-06-15 11:47:25 +0700

    Add Blog page

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-class post(models.Model):
+class Post(models.Model):
 	post_title = models.CharField(max_length=300)
 	post_date = models.DateTimeField()
 	post_image = models.ImageField(upload_to='blog_images/')
 	post_text = models.TextField()
+
+	def get_summary(self):
+		return self.post_text[:70]
